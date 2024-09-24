99d4fca480f7741b0693b59080724ae14ecb6df6
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2024-09-24 17:46:57 +0200

    chores: remove unuseful files

--- tomd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-src=${1}
-
-echo "| switch | variable |preproc | location |" > README.md
-echo "| -------| -------- |------- | -------- |" >> README.md
-find ${src} -type f -name "*switch*.cc" -exec python3 main.py "{}" \; \
-    | sort  | sed -e 's/^/|/' | sed -e 's/$/|/' | tr ',' '|' >> README.md
