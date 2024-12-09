36aaf0782fd2d43acb1171b2ef15c142c0ecc651
RANDRIANAINA Georges Aaron <randrianaina.georgesaaron@gmail.com>
2024-12-09 18:03:35 +0100

    basic tasks, nothing yet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use tree_sitter::{Language, Parser};
+use tree_sitter_cpp::{LANGUAGE};
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut parser = Parser::new();
+    let language = LANGUAGE;
+    parser
+	.set_language(&language.into())
+	.expect("Error loading C++ parser");
+    let filename = std::env::args().nth(1).expect("No filename provided");
+    let content = fs::read_to_string(filename).expect("Failed to read the file");
+    let mut tree = parser.parse(content, None).unwrap();
+    let root_node = tree.root_node();
 }
